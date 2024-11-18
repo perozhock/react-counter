19e0aa32a61742356ed7a6ae38f0736a5edf72f7
perozhock <shat.prog1@gmail.com>
2024-11-18 22:32:10 +0300

    new logic, animation

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,48 @@
-import { useState } from "react"
+import './styles/Counter.css';
+import { useState } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const [animate, setAnimate] = useState(false);
 
-    function plus() {
-        setCount(count => count + 1);
-    }
-      
-    function minus() {
-        setCount(count => count - 1);
-    }
+    const handleChange = (delta) => {
+        setCount((prevCount) => {
+            const newCount = prevCount + delta;
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 300);
+            return newCount;
+        });
+    };
 
-      return (
+    const resetCounter = () => {
+        setCount(0);
+        setAnimate(true);
+        setTimeout(() => setAnimate(false), 300);
+    };
+
+    return (
         <div className="counter">
-            <div className="counter__name" >Счетчик</div>
+            <h1 className="counter__name">Счётчик</h1>
             <div className="counter__content">
-                <h1>Число: {count}</h1>
+                <h2 className={`number ${animate ? 'animate' : ''}`}>Число: {count}</h2>
             </div>
             <div className="counter__buttons">
-                <button onClick={plus}>+</button>
-                <button onClick={minus}>-</button>
+                <button onClick={() => handleChange(1)} aria-label="Увеличить">
+                    +
+                </button>
+                <button
+                    onClick={() => handleChange(-1)}
+                    // disabled={count <= 0}
+                    aria-label="Уменьшить"
+                >
+                    -
+                </button>
+                <button onClick={resetCounter} aria-label="Сбросить">
+                    Сброс
+                </button>
             </div>
         </div>
-      )
+    );
+};
 
-}
-
-export default Counter
+export default Counter;
