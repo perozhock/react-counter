5e5fd55808ab001736e9d1ac87b462be67bb04b9
perozhock <shat.prog1@gmail.com>
2024-11-22 18:27:57 +0300

    add styles, minor changes

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,66 +1,65 @@
 import './styles/ToDo.css'
 import { useState } from "react"
 
-const data = [
-    {
-        id: 1,
-        title: "-",
-        isComlete: false    
-    }
-]
-
 const InputForm = ({onAdd}) => {
     const [name, setName] = useState("");
+
     function handleKey(ev) {
         if (ev.key === 'Enter' && name.trim() !== '') {
-            onAdd(name);
+            onAdd(name.trim());
             setName('');
         }
     }
+
     return (
         <div className="input" onKeyDown={handleKey}>
-        <input 
-        type="text" 
-        placeholder="add todo" 
-        onChange={ev => setName(ev.target.value)}
-        value={name}
-        />
+            <input 
+                type="text" 
+                placeholder="add todo" 
+                onChange={ev => setName(ev.target.value)}
+                value={name}
+                onKeyDown={handleKey}
+            />
         </div>
-        )
+    )
 }
 
-const TodoI = ({item, onli}) => {
+const TodoI = ({item, onToggle}) => {
     return (
-    <div>
-    <button key={item.id} onClick={onli}
-    className={item.isComlete ? "item-text strike" : "item-text"}>
-        {item.title} {item.id}
-    </button>
-    </div>
+        <div className='todo-item'>
+            <button 
+                className={`item-text ${item.isComplete ? 'strike' : ''}`}
+                onClick={() => onToggle(item.id)}
+            > {item.title}
+            </button>
+        </div>
     )
 }
 
 const TodoList = () => {
-    const [todo, setTodo] = useState(data)
-    
-    let id_n = todo[0].id;
+    const [todo, setTodo] = useState([]);
+    const [idCounter, setIdCounter] = useState(1);
 
-    function addTask(title) {
-        setTodo([{id:id_n + 1, title, isComlete:false}, ...todo])
+    const addTask = (title) => {
+        setTodo([{ id: idCounter, title, isComplete: false}, ...todo]);
+        setIdCounter((prevId) => prevId + 1);
     }
 
-    function toogleTask(id) {
-        setTodo([
-            ...todo.map((todo) => 
-        todo.id === id ? {...todo, isComlete: !todo.isComlete} : {...todo})
-        ])
-    }
+    const toggleTask = (id) => {
+        setTodo((prevTodo) =>
+            prevTodo.map((task) =>
+                task.id === id ? {...task, isComplete: !task.isComplete } : task
+            )
+        );
+    };
 
     return (
         <div className="todo">
             <div className="todo-name">Список дел</div>
             <InputForm onAdd={addTask} />
-            {todo.map(item => (<TodoI key={item.id} item={item} onli={() => toogleTask(item.id)} />))}
+            {todo.map((item) => (
+                <TodoI key={item.id} item={item} onToggle={toggleTask} />
+            ))}
         </div>
     )
 }
